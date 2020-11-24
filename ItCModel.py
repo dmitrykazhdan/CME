e2ec3be64f35e9826babd94cb30b7926f823970d
Dmitry Kazhdan <dmitry@kazhdan.net>
2020-11-24 22:58:45 +0000

    upload concept extractor code

--- ItCModel.py
+++ ItCModel.py
@@ -1,146 +1,170 @@
-from abc import ABC, abstractmethod
-import matplotlib.pyplot as plt
-import os
-from sklearn.manifold import TSNE
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.semi_supervised import LabelSpreading
+import tensorflow as tf
+from sklearn.dummy import DummyClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
-from utils import aggregate_activations, compute_activation_per_layer
+from utils import compute_activation_per_layer, flatten_activations
 
+class ItCModel():
 
-class ItCModel(ABC):
-
-    @abstractmethod
     def __init__(self, model, **params):
-        '''
-        :param model:    DNN model concepts are extracted from
-        :param c_data_l: Concept data
-        :param params:   Extra parameters
-        '''
 
         # Create copy of passed-in parameters
         self.params = params
 
-        # Specify name of concept extractor
-        self.name = self.params.get("name", "ConceptExtractor")
-
         # Retrieve the layers to use for hidden representations
-        if "layer_ids" in self.params:
-            self.layer_ids = self.params["layer_ids"]
-        else:
-            self.layer_ids = [i for i in range(len(model.layers))]
+        self.layer_ids = params.get("layer_ids", [i for i in range(len(model.layers))])
 
         # Retrieve the corresponding layer names
-        if "layer_names" in self.params:
-            self.layer_names = self.params["layer_names"]
-        else:
-            self.layer_names = ["Layer " + str(i) for i in range(len(model.layers))]
+        self.layer_names = params.get("layer_names", ["Layer " + str(i) for i in range(len(model.layers))])
 
+        # Set number of parameters
         self.n_concepts = params['n_concepts']
 
         # Retrieve the concept names
-        if "concept_names" in self.params:
-            self.concept_names = self.params["concept_names"]
-        else:
-            self.concept_names = ["Concept " + str(i) for i in range(self.n_concepts)]
-
-        # Retrieve the concepts to use at each layer
-        self.layer_concepts = []
+        self.concept_names = params.get("concept_names", ["Concept " + str(i) for i in range(self.n_concepts)])
 
         # Batch size to use during computation
         self.batch_size = params.get("batch_size", 128)
 
-        # Specify number of workers to use, for parallelism
-        workers = os.cpu_count()
-        workers = workers // 2
-        workers = max(1, workers)
-        self.workers = workers
-
         # Set classifier to use for concept value prediction
         self.clf_method = params.get("method", "LR")
 
+        # Model for extracting activations from
         self.model = model
 
 
     def get_clf(self):
-        '''
-        Specify which classifier to use for concept value prediction
-        :return:
-        '''
-
         if self.clf_method == 'LR':
-            clf = LogisticRegression(max_iter=200, n_jobs=self.workers)
+            semi_supervised = False
+            clf = LogisticRegression(max_iter=200)
         elif self.clf_method == 'LP':
+            semi_supervised = True
             clf = LabelSpreading()
         else:
             raise ValueError("Non-implemented method")
 
-        return clf
+        return clf, semi_supervised
 
 
-    @abstractmethod
+    def _get_layer_concept_predictor_model(self, h_data_l, c_data_l, h_data_u):
+        '''
+        Train cme concept label predictor for a particular layer and concept
+        :param h_data_l: Activation data for a given layer
+        :param c_data_l: Corresponding concept labels for that data
+        :param h_data_u: Activation data without corresponding labels
+        :return: Classifier predicting concept values from the activations
+        '''
+
+        # Create safe copies of data
+        x_data_l = np.copy(h_data_l)
+        y_data_l = np.copy(c_data_l)
+        x_data_u = np.copy(h_data_u)
+        y_data_u = np.ones((x_data_u.shape[0])) * -1    # Here, label is 'undefined'
+
+        # Specify whether to use the unlabelled data at all
+        semi_supervised = False
+
+        # If there is only 1 value, then return simple classifier
+        unique_vals = np.unique(y_data_l)
+
+        if len(unique_vals) == 1:
+            clf = DummyClassifier(strategy="constant", constant=c_data_l[0])
+        else:
+            # Otherwise, train classifier model
+            clf, semi_supervised = self.get_clf()
+
+        # Split the labelled data for train/test
+        x_train, x_test, y_train, y_test = train_test_split(x_data_l, y_data_l, test_size=0.15)
+
+        # Combine with unlabelled data, if using a semi-supervised method
+        if semi_supervised:
+            x_train = np.concatenate([x_train, x_data_u])
+            y_train = np.concatenate([y_train, y_data_u])
+
+        # Train classifier
+        clf.fit(x_train, y_train)
+
+        # Retrieve predictive accuracy of classifier
+        pred_acc = accuracy_score(y_test, clf.predict(x_test))
+
+        return clf, pred_acc
+
+
+    def train(self, x_data_l, c_data_l, x_data_u):
+        '''
+        Compute a dictionary with structure: layer --> concept --> model
+        i.e., the dictionary returns which clf to use to predict a given concept, from a given layer
+        '''
+
+        self.model_ensemble = {}
+        self.model_accuracies = {}
+
+        # Compute activations for specified layers
+        h_data_ls = compute_activation_per_layer(x_data_l, self.layer_ids, self.model, aggregation_function=flatten_activations)
+        h_data_us = compute_activation_per_layer(x_data_u, self.layer_ids, self.model, aggregation_function=flatten_activations)
+
+        for i, layer_id in enumerate(self.layer_ids):
+
+            # Retrieve activations for next layer
+            activations_l = h_data_ls[i]
+            activations_u = h_data_us[i]
+
+            output_layer = self.model.layers[layer_id]
+            self.model_ensemble[output_layer] = []
+            self.model_accuracies[output_layer] = []
+
+            # Generate predictive models for every concept
+            for c in range(self.n_concepts):
+                clf, pred_acc = self._get_layer_concept_predictor_model(activations_l, c_data_l[:, c], activations_u)
+                self.model_ensemble[output_layer].append(clf)
+                self.model_accuracies[output_layer].append(pred_acc)
+
+            print("Processed layer ", str(i + 1), " of ", str(len(self.layer_ids)))
+
+        # Initialise concept predictors with models in the first layer
+        # Consists of an array of size |concepts|, in which
+        # The element arr[i] is the Layer Id of the layer to use when predicting that concept
+        self.concept_predictor_layer_ids = [self.layer_ids[0] for _ in range(self.n_concepts)]
+
+        # For every concept, identify the layer with the best clf predictive accuracy
+        for c in range(self.n_concepts):
+            max_acc = 0
+            for i, layer_id in enumerate(self.layer_ids):
+                layer = self.model.layers[layer_id]
+                acc = self.model_accuracies[layer][c]
+
+                if acc > max_acc:
+                    max_acc = acc
+                    self.concept_predictor_layer_ids[c] = layer_id
+
+
     def predict_concepts(self, x_data):
         '''
-        Predict concept values from input data
-        :return:
-        '''
-        pass
-
-    @abstractmethod
-    def _extract_concepts(self, x_data_l, c_data_l, x_data_u, y_data_l):
-        """
-        Train the concept extraction model
-
-        :param x_data_l: labelled input data
-        :param c_data_l: labelled concept data
-        :param x_data_u: unlabelled input data
-        :return:
-        """
-        pass
-
-
-    def compute_tsne_embedding(self, x_data):
-        '''
-        Compute tSNE latent space embeddings for specified layers of the DNN model
-        :param x_data:
-        :return:
+        Given the same model and new x_data, predict their concept values
         '''
 
-        h_l_list_agg = compute_activation_per_layer(x_data, self.layer_ids, self.model,
-                                                    self.batch_size,
-                                                    aggregation_function=aggregate_activations)
-        h_l_embedding_list = []
+        n_samples = x_data.shape[0]
+        concept_vals = np.zeros((n_samples, self.n_concepts), dtype=float)
 
-        for i, h_l in enumerate(h_l_list_agg):
-            h_embedded = TSNE(n_components=2, n_jobs=4).fit_transform(h_l)
-            h_l_embedding_list.append(h_embedded)
-            print(self.layer_names[i])
-        return h_l_embedding_list
+        for c in range(self.n_concepts):
+            # Retrieve clf corresponding to concept c
+            layer_id = self.concept_predictor_layer_ids[c]
+            output_layer = self.model.layers[layer_id]
+            clf = self.model_ensemble[output_layer][c]
 
-    def visualise_hidden_space(self, x_data, c_data):
+            # Compute activations for that layer
+            output_layer = self.model.layers[layer_id]
+            reduced_model = tf.keras.Model(inputs=self.model.inputs, outputs=[output_layer.output])
+            hidden_features = reduced_model.predict(x_data, batch_size=self.batch_size)
+            clf_data = flatten_activations(hidden_features)
+            concept_vals[:, c] = clf.predict(clf_data)
 
-        # Compute tSNE embeddings
-        h_l_embedding_list = self.compute_tsne_embedding(x_data)
+        return concept_vals
 
-        # Create figure of size |n_concepts| * |n_layers|
-        n_concepts = len(self.concept_names)
-        n_rows = n_concepts
-        n_cols = len(h_l_embedding_list)
-        fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=(3 * n_cols, 4 * n_rows))
-
-        # Plot the embeddings of every layer, highlighting concept values
-        for i, h_2 in enumerate(h_l_embedding_list):
-            for j in range(1, n_concepts):
-                ax = axes[j-1, i]
-                ax.scatter(h_2[:, 0], h_2[:, 1], c=c_data[:, j])
-                ax.set_title(self.layer_names[i], fontsize=20)
-                ax.set_xticks([])
-                ax.set_yticks([])
-
-                if i == 0:
-                    ax.set_ylabel(self.concept_names[j], fontsize=20)
-
-        return fig
 
 
 
